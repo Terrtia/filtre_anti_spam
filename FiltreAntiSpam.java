d04f2c78d733e1a21168fe3777b67e06f0dad544
Terrtia <or1994@hotmail.fr>
2017-04-07 14:24:53 +0200

    add FiltreAntiSpam.java

---
+++ FiltreAntiSpam.java
@@ -0,0 +1,17 @@
+package filtre_anti_spam;
+
+public class FiltreAntiSpam {
+
+	public FiltreAntiSpam() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
